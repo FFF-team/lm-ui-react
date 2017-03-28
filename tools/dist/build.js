ef57191e051de359b2e798bf0abbce77c738254d
孟祥瑞 <429217743@qq.com>
2017-03-28 11:43:52 +0800

    FEA:add tools

---
+++ tools/dist/build.js
@@ -0,0 +1,17 @@
+import 'colors';
+import { exec } from '../exec';
+import fsp from 'fs-promise';
+import { srcRoot, distRoot } from '../constants';
+import buildBabel from '../buildBabel';
+console.log('test');
+export default function BuildDistributable() {
+  	console.log('Building: '.cyan + 'dist'.green);
+
+  	return exec(`rimraf ${distRoot}`)
+    .then(() => Promise.all([
+      	exec(`webpack`),
+        exec(`webpack -p`)
+    ]))
+    .then(() => console.log('Built: '.cyan + 'dist'.green))
+
+}
---
+++ tools/lib/build.js
@@ -0,0 +1,15 @@
+import 'colors';
+import { exec } from '../exec';
+import fsp from 'fs-promise';
+import { srcRoot, libRoot } from '../constants';
+import buildBabel from '../buildBabel';
+console.log('s')
+
+export default function BuildCommonJs() {
+  console.log('Building: '.cyan + 'npm module'.green);
+
+  return exec(`rimraf ${libRoot}`)
+    .then(() => fsp.mkdirs(libRoot))
+    .then(() => buildBabel(srcRoot, libRoot))
+    .then(() => console.log('Built: '.cyan + 'npm module'.green));
+}
