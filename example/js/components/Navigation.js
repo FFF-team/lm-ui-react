ab671135733c2dbf81894e2b7413c99bc83817c1
zihong <972836897@qq.com>
2017-03-30 10:00:20 +0800

    按钮ui

--- example/js/components/Navigation.js
+++ example/js/components/Navigation.js
@@ -15,7 +15,7 @@ export default class Navigation extends React.Component {
 
 					<li><Link to={{ pathname: '/UseAlert', query: { name: '弹出框' } }} className="list-item" >弹出框</Link></li>
 
-					<li><Link to='/UseAlert' className="list-item" >弹出框</Link></li>
+					<li><Link to={{ pathname: '/Button', query: { name: '按钮' } }} className="list-item" >按钮</Link></li>
 
 					<li><Link to='/UseAlert' className="list-item">弹出框</Link></li>
 
