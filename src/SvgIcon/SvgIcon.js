53bb1df82c4895785d341cb135ae9ac47af85841
kanghongyan <khongyan379@sina.com>
2017-04-07 17:01:21 +0800

    add SvgIcon.test.js

---
+++ src/SvgIcon/SvgIcon.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+class SvgIcon extends React.Component {
+    static lmuiName = 'SvgIcon';
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            hovered: false
+        }
+    }
+    
+    render() {
+        const {
+            children,
+            color,
+            style,
+            viewBox,
+            ...other
+        } = this.props;
+        
+        
+        const mergedStyle = Object.assign({
+            display: 'inline-block',
+            color: '#333',
+            fill: color,
+            height: 24,
+            width: 24,
+            userSelect: 'none',
+        }, style);
+        
+        return (
+            <svg { ...other }
+                 style={ mergedStyle }
+                 viewBox={ viewBox }
+            >
+                { children }
+            </svg>
+        )
+    }
+}
+
+SvgIcon.propTypes = {
+    children: React.PropTypes.node,
+    color: React.PropTypes.string,
+    viewBox: React.PropTypes.string,
+    style: React.PropTypes.object
+};
+
+export default SvgIcon
---
+++ src/SvgIcon/SvgIcon.test.js
@@ -0,0 +1,37 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react'
+import ReactDom from 'react-dom'
+import SvgIcon from './SvgIcon'
+
+describe('<SvgIcon />', () => {
+    const pathDom = <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>;
+    
+    test('render without crashing', () => {
+        const div = document.createElement('div');
+        ReactDom.render(<SvgIcon/>, div)
+    });
+    
+    test('render children and color', () => {
+        const wrapper = shallow(<SvgIcon color="#eee">{ pathDom }</SvgIcon>);
+        
+        expect(wrapper.contains(pathDom)).toBeTruthy();
+        expect(wrapper.get(0).props.style.fill).toEqual('#eee');
+    });
+    
+    test('render overwrite style', () => {
+        const style = {
+            backgroundColor: 'green',
+            width: '30px'
+        };
+        const wrapper = shallow(
+            <SvgIcon style={ style }>
+                { pathDom }
+            </SvgIcon>
+        )
+        
+        const wrapperStyle = wrapper.get(0).props.style;
+        
+        expect(wrapperStyle.width).toEqual(style.width);
+        expect(wrapperStyle.backgroundColor).toEqual(style.backgroundColor);
+    })
+});
