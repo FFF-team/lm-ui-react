c69acacc6536870057cff9193f2dc587ac0b422f
fuhebo <fuhebo@58ganji.com>
2017-09-08 14:56:21 +0800

    修改dialog

--- example/js/components/UserDialog.js
+++ example/js/components/UserDialog.js
@@ -1,7 +1,103 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Dialog } from 'src/index.js';
 import TopBanner from './TopBanner';
-import { ActionSheet } from 'src'
+import { ActionSheet, Input, FormGroup } from 'src'
+
+
+const Custom = (props) => {
+	return (
+		<div className='custom-my-dialog'>
+			<dl>
+				<dt>还款方式：等本等息</dt>
+				<dd>1、主动还款：请及时登录查看还款计划，在每期还款日之前点击还款</dd>
+				<dd>2、银行代扣：还款日当天17:00从绑定的银行卡中自动扣款</dd>
+				<dt>提前还款</dt>
+				<dd>一次性提前还款：暂不支持</dd>
+				<dt>逾期政策</dt>
+				<dd>罚息：逾期费率0.1%/天，暂时不计复息</dd>
+			</dl>
+			<p>如有疑问，可致电58金融客服：<a href="tel:10105800">10105800</a></p>
+		</div>
+	)
+};
+
+const customDefaultProps = {
+	closeFun: PropTypes.func
+};
+
+const customPropTypes = {
+	closeFun: () => {}
+};
+
+Custom.defaultProps = customDefaultProps;
+Custom.propTypes = customPropTypes;
+
+const CustomFooterConfirm = (props) => {
+
+	const { 
+			defaultText,
+			value,
+			cleanFun,
+            onChange,
+            promptText,
+            validateFun,
+            smsSecond,
+            onSmsClick } = props;
+
+	return (
+
+		<div className="costom-dialog">
+			<div className="costom-top-text">
+				<p>短信验证码</p>
+				<p>招商银行储蓄卡(1234)</p>
+			</div>
+		 	<FormGroup>
+			 	<Input 
+					type='input'
+					value={ value }
+					validateData={{ promptText: promptText, validateFun: (value) => { return validateFun(value) } }}
+					cleanBtn={{ state: true, cleanFun: () => { cleanFun() } }}
+					onChange={ onChange }
+					defaultText={ defaultText } />
+				<a href="javascript:;" 
+					className={ smsSecond === 0 ? "lm-ui-get-code-btn" : 'lm-ui-get-code-btn-disable'} 
+					onClick= {smsSecond === 0 ? (e) => { onSmsClick } : null } >
+					{smsSecond === 0 ? '获取验证码' : smsSecond + 's后重发'}
+				</a>
+			</FormGroup>
+			<p className="costom-bottom-text">1212121212121212</p>
+		</div>
+
+	)
+
+};
+
+const propTypes = {
+	defaultText: PropTypes.string,
+	value: PropTypes.any.isRequired,
+	cleanFun: PropTypes.func,
+    onChange: PropTypes.func,
+	promptText: PropTypes.string,
+	validateFun: PropTypes.func,
+	smsSecond: PropTypes.number,
+	onSmsClick: PropTypes.func
+};
+
+const defaultProps = {
+	defaultText: '请输入短信验证码',
+	value: null,
+	cleanFun: () => {},
+    onChange: () => {},
+	promptText: '请输入正确的验证码',
+	validateFun: () => {},
+	smsSecond: 0,
+	onSmsClick: () => {}
+
+};
+
+CustomFooterConfirm.defaultProps = defaultProps;
+CustomFooterConfirm.propTypes = propTypes;
 
 export default class UserDialog extends React.Component {
 
@@ -15,7 +111,9 @@ export default class UserDialog extends React.Component {
 
 	    	dialogType: "Alert",
 			
-			actionState: false
+			actionState: false,
+
+			smsValue: 1986
 
 	    }
 	
@@ -61,7 +159,9 @@ export default class UserDialog extends React.Component {
 
 							<a href="javascript:;" 
 							 	className="list-item"
-								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "Alert"})}}>
+								onClick={()=>{
+									this.setState({dialogState: !this.state.dialogState, dialogType: "Alert", dialogStyle: null, styleObject: null })
+								}}>
 								alert测试
 							</a>
 
@@ -71,7 +171,7 @@ export default class UserDialog extends React.Component {
 
 							<a href="javascript:;" 
 							 	className="list-item"
-								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "Confirm"})}}>
+								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "Confirm", dialogStyle: null, styleObject: null })}}>
 								confirm测试
 							</a>
 
@@ -81,10 +181,65 @@ export default class UserDialog extends React.Component {
 							
 							<a href="javascript:;"
 							   className="list-item"
-							   onClick={()=>{this.setState({actionState: !this.state.actionState})}}>
+							   onClick={()=>{this.setState({actionState: !this.state.actionState, dialogStyle: null, styleObject: null, dialogStyle: null, styleObject: null})}}>
 								功能操作
 							</a>
 							
+						</li>
+
+						<li>
+
+							<a href="javascript:;" 
+							 	className="list-item"
+								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "Custom", footerType: 'Confirm', headerType: null, type: null, dialogStyle: null, styleObject: null })}}>
+								Custom + footerType 测试
+							</a>
+
+						</li>
+
+						<li>
+
+							<a href="javascript:;" 
+							 	className="list-item"
+								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "CustomPrompt", dialogStyle: null, styleObject: null })}}>
+								CustomPrompt
+							</a>
+
+						</li>
+						<li>
+
+							<a href="javascript:;" 
+							 	className="list-item"
+								onClick={()=>{this.setState({
+									dialogState: !this.state.dialogState, 
+									dialogType: "Custom", 
+									footerType: 'Close', 
+									headerType: 'CustomPrompt', 
+									dialogStyle: { background: 'rgba(255, 255, 255, 0.95)', position: "fixed", top: 0, bottom: 0, width: "inherit", maxWidth: 'inherit' },
+									styleObject: { backgroundColor: 'transparent' },
+									type: 'prompt' 
+								})}}>
+								Custom
+							</a>
+
+						</li>
+						<li>
+
+							<a href="javascript:;" 
+							 	className="list-item"
+								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "Custom", headerType: "CustomForm", footerType: "Confirm", type: null, dialogStyle: null, styleObject: null })}}>
+								Custom + AlertHeader + FooterConfirm
+							</a>
+
+						</li>
+						<li>
+
+							<a href="javascript:;" 
+							 	className="list-item"
+								onClick={()=>{this.setState({dialogState: !this.state.dialogState, dialogType: "CustomForm", dialogStyle: null, styleObject: null })}}>
+								CustomForm
+							</a>
+
 						</li>
 
 					</ul>
@@ -92,13 +247,37 @@ export default class UserDialog extends React.Component {
 				</div>
 
 				<Dialog 
+					className = "121212"
 					dialogType={this.state.dialogType}
 					showState={this.state.dialogState}
 					headText={'提示标题'}
-					contentText={'内容'}
+					modelStyle={ this.state.styleObject }
+					dialogStyle={ this.state.dialogStyle }
+					contentText={ this.state.contentText }
+					footerType={this.state.footerType}
+					headerType={this.state.headerType}
 					btnCommonFun={() => {this.setState({dialogState: false})}}
 					btnLeftCbFun={() => {console.log('left fun')}}
-					btnRightCbFun={() => {console.log('right fun')}}/>
+					btnRightCbFun={() => {console.log('right fun')}}>
+					{
+						this.state.type === 'prompt' ?
+						<Custom /> :
+						<CustomFooterConfirm 
+							defaultText={ '121212' }
+							value={ this.state.smsValue }
+							cleanFun={ (e) => {
+								this.setState({ smsValue: '' })
+							} }
+		            		onChange={ (e) => { 
+		            		 this.setState({ smsValue: e.value }) 
+		            		} }
+		            		promptText={ '12ddddd' }
+		            		validateFun={ (value) => { return false; } }
+		            		onSmsClick={ (e) => { console.log(121212) } }
+		            		smsSecond={ 0 }/>
+					}
+					
+				</Dialog>
 				
 				<ActionSheet
 					showState={ this.state.actionState }
--- src/Dialog/index.js
+++ src/Dialog/index.js
@@ -6,7 +6,9 @@ import ModelHOC from '../ModelHOC/index.js';
 /*
  * props:
  *  showState HOC
- *  dialogType
+ *  dialogType Alert Confirm CustomPrompt CustomForm Custom
+ *  headerType Alert Confirm CustomPrompt CustomForm
+ *  footerType Alert Confirm Close
  *	headText
  *	contentText
  *	btnLeftText
@@ -14,33 +16,52 @@ import ModelHOC from '../ModelHOC/index.js';
  *  btnLeftCbFun
  *  btnRightCbFun
  *  btnCommonFun
+ *  btnCloseFun
  */
+const TypeList = ["Alert", "Confirm", "CustomPrompt", "CustomForm", "Custom"];
+
+const HeaderLit = ["Alert", "Confirm", "CustomPrompt", "CustomForm"];
+
+const FooterList = ["Alert", "Confirm", "Close"];
 
 const propTypes = {
-	dialogType: PropTypes.string.isRequired,
+	modelStyle: PropTypes.object,
 	showState: PropTypes.bool.isRequired,
 	opacity: PropTypes.number,
+	dialogStyle: PropTypes.object,
+	dialogType: PropTypes.string.isRequired,
+	headerType: PropTypes.string,
+	headerStyle: PropTypes.object,
     headText: PropTypes.node,
+    contentStyle: PropTypes.object,
 	contentText: PropTypes.node,
+	footerType: PropTypes.string,
 	btnLeftText: PropTypes.string,
 	btnRightText: PropTypes.string,
 	btnCommonFun: PropTypes.func,
 	btnLeftCbFun: PropTypes.func,
-	btnRightCbFun: PropTypes.func
-
+	btnRightCbFun: PropTypes.func,
+	btnCloseFun: PropTypes.func
 };
 
 const defaultProps = {
+	modelStyle: {},
+	dialogStyle: {},
+	headerStyle: {},
+	contentStyle: {},
 	dialogType: "Confirm",
 	opacity: 5,
 	showState: false,
     headText: '提示',
+    headerType: "",
+    footerType: "",
 	contentText: '提示标题',
 	btnLeftText: '取消',
 	btnRightText: '确定',
 	btnCommonFun: () => {},
 	btnLeftCbFun: () => {},
-	btnRightCbFun: () => {}
+	btnRightCbFun: () => {},
+	btnCloseFun: () => {}
 
 };
 
@@ -48,59 +69,132 @@ const Dialog = (props) => {
 
 	const { 
 			dialogType,
-			opacity,
 			showState,
+			headerType,
+			headerStyle,
+			contentStyle,
             headText,
+            footerType,
 			contentText,
 			btnLeftText,
-			btnRightText } = props;
+			btnRightText,
+			dialogStyle } = props;
+
+	if(TypeList.indexOf(dialogType) < 0) {
+		alert("请传入正确dialogType！");
+		return;
+	}
+	if(headerType && HeaderLit.indexOf(headerType) < 0) {
+		alert("请传入正确headerType！");
+		return;
+	}
+	if(footerType && FooterList.indexOf(footerType) < 0) {
+		alert("请传入正确footerType！");
+		return;
+	}
 
 	const clickHandler = (dir) => {
 
 		const { btnLeftCbFun,
 				btnRightCbFun,
-				btnCommonFun } = props;
+				btnCommonFun,
+				btnCloseFun } = props;
 
 		btnCommonFun();
 
-		dir === 'left' ? btnLeftCbFun() : (dir === 'right' ? btnRightCbFun() : null);
+		dir === 'left' ? btnLeftCbFun() : dir === 'right' ? btnRightCbFun() : dir === 'close' ? btnCloseFun() : null;
 
 	}
 
+
+
+	let dialogClass = "lm-ui-dialog";
+	let headerClass = "dialog-head";
+	let footerClass = "dialog-btns";
+	let contentClass = "dialog-content";
+	let headerTypeStr = "alert";
+	let footerTypeStr = "alert";
+
+	switch(dialogType) {
+		case "Confirm":
+			headerTypeStr = "confirm";
+			footerTypeStr = "confirm";
+			break;
+		case "CustomPrompt":
+			headerClass += " dialog-header-prompt";
+			footerClass += " dialog-btn-prompt";
+			contentClass += " dialog-prompt-content";
+			dialogClass += " lm-ui-dialog-full";
+			headerTypeStr = "prompt";
+			footerTypeStr = "prompt";
+			break;
+		case "CustomForm":
+			headerClass += " dialog-header-form";
+			headerTypeStr = "form";
+			dialogClass += " lm-ui-dialog-full";
+			footerTypeStr = "custom";
+			break;
+		case "Custom":
+			headerType === "Confirm" && (headerTypeStr = "confirm");
+			footerType === "Confirm" && (footerTypeStr = "confirm");
+			footerType === "Close" && (dialogClass += " lm-ui-dialog-full", 
+				footerTypeStr = "prompt",
+				footerClass += " dialog-btn-prompt"
+			);
+			headerType === "CustomPrompt" && (headerClass += " dialog-header-prompt",
+				contentClass += " dialog-prompt-content",
+				headerTypeStr = "prompt"
+			);
+			headerType === "CustomForm" && (headerClass += " dialog-header-form",
+				headerTypeStr = "form"
+			);
+			!footerType && (footerTypeStr = "custom");
+			!headerType && (headerTypeStr = "custom");
+	}
+
+	let Header = headerTypeStr !== "custom" ? (<div className={ headerClass } style={ headerStyle }>
+					{ 
+						headerTypeStr === "form" || headerTypeStr === "prompt" ?
+						(<p className="header-form-title">{ headText }</p>) :
+						headText
+					}
+					{
+						headerTypeStr === "confirm" || headerTypeStr === "form" ?
+						(<i className="header-close" onClick={clickHandler.bind(null, 'close')}></i>) : null
+					}
+				 </div>) : null;
+
+
+
+	let Footer = footerTypeStr !== "custom" ? (<div className={ footerClass }>
+
+					{ footerTypeStr === "confirm" ? 
+						<a  href="javascript:;" 
+							className="dialog-btn"
+							onClick={clickHandler.bind(null, 'left')}>
+							{btnLeftText}
+						</a> : null
+					}
+
+					<a  href="javascript:;" 
+						className="dialog-btn special"
+						onClick={clickHandler.bind(null, footerTypeStr === "prompt" ? 'close' : 'right')}>
+						{ footerTypeStr === "prompt" ? "" : btnRightText }
+					</a>
+
+				 </div>) : null;
+
+	let Content = dialogType ==='Alert' || dialogType==='Confirm' || dialogType === 'CustomPrompt' ?
+				  (<div className={ contentClass } style={ contentStyle }> { contentText } </div>) :
+				  ( props.children );
+
 	return (
 
-			<div className="lm-ui-dialog">
-				
-				<div className="dialog-head">
-					{ headText }
-				</div>
-
-				
-
-				<div className="dialog-content">
-					{ contentText }
-				</div>
-
-				<div className="dialog-btns">
-					{ dialogType === "Confirm" ? (
-					<a 
-						href="javascript:;" 
-						className="dialog-btn"
-						onClick={clickHandler.bind(null, 'left')}>
-						{btnLeftText}
-					</a>
-					) : null }
-					
-					<a 
-						href="javascript:;" 
-						className="dialog-btn special"
-						onClick={clickHandler.bind(null, 'right')}>
-						{btnRightText}
-					</a>
-
-				</div>
-
-			</div>
+		 <div className={ dialogClass } style={ dialogStyle }>
+		 	{ Header }
+		 	{ Content }
+		 	{ Footer }
+		</div>
 
 	)
 
